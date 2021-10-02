463f66ef44203ccc2120f6d2945901feee2b93ff
Daniele <danieleslvstrn@gmail.com>
2021-10-02 16:30:24 +0200

    changes to exercise number 5

--- 5-units-of-time-again/5-units-again.py
+++ 5-units-of-time-again/5-units-again.py
@@ -7,7 +7,7 @@ def calc_days(seconds):
         hours_rest_1 = days_rest % 3600
         minute_1 = hours_rest_1 // 60
         minute_rest_1 = hours_rest_1 % 60
-        print("The amount of seconds you typed is equal to:\n" + str(days).zfill(2) + ":" + str(hours_1).zfill(2) + ":" + str(minute_1).zfill(2) + ":" + str(minute_rest_1).zfill(2)) 
+        print("The amount of seconds you typed is equal to (D:HH:MM:SS):\n" + str(days).zfill(2) + ":" + str(hours_1).zfill(2) + ":" + str(minute_1).zfill(2) + ":" + str(minute_rest_1).zfill(2)) 
    
 calc_days(seconds)
 
